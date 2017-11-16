8992c410412c8d692a0dd8bba70daea7bd666e76
Kostas Christopoulos <konstadinoschr@gmail.com>
2017-11-17 01:50:22 +0200

    Added collective  html. Pending: fixes in routing.

--- config/app.php
+++ config/app.php
@@ -165,7 +165,6 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        Mews\Purifier\PurifierServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -179,7 +178,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class
     ],
 
     /*
@@ -228,10 +226,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form'      => Collective\Html\FormFacade::class,
-        'Html'      => Collective\Html\HtmlFacade::class,
-        'Image'     => Intervention\Image\Facades\Image::class,
-        'Purifier'  => Mews\Purifier\Facades\Purifier::class,
 
     ],
 
